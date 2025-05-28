8543de17fddb2100609bccd5582857d49404c71f
Arne Skaar Fismen <arnefismen@gmail.com>
2025-05-28 15:25:08 +0200

    initial commit
    initial commit
    
    Update vars via generator zerotier for machine cube
    
    Update vars via generator user-password for machine cube
    
    Update vars via generator state-version for machine cube
    
    Update vars via generator root-password for machine cube
    
    Update vars via generator openssh for machine cube
    
    Update vars via generator disk-id for machine cube
    
    Add machine cube to secrets
    
    Update secret cube-age.key

---
+++ machines/cube/configuration.nix
@@ -0,0 +1,24 @@
+{
+  imports = [
+    # contains your disk format and partitioning configuration.
+    ../../modules/disko.nix
+    # this file is shared among all machines
+    ../../modules/shared.nix
+  ];
+
+  users.users.user.name = "arne";
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJkHOi39HCigHCOneTKIiY+C809n6d3sNHd3hoy2Uq21 Warden"
+  ];
+  clan.core.networking.targetHost = "root@10.10.10.10";
+
+  # You can get your disk id by running the following command on the installer:
+  # Replace <IP> with the IP of the installer printed on the screen or by running the `ip addr` command.
+  # ssh root@<IP> lsblk --output NAME,ID-LINK,FSTYPE,SIZE,MOUNTPOINT
+  disko.devices.disk.main.device = "/dev/disk/by-id/nvme-eui.00000000000000000026b7382faa1195";
+
+
+
+
+  clan.core.networking.zerotier.controller.enable = true;
+}
